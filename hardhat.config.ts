0db06d5626a55d0b4db5a54df6d56a37b403bf9d
jaebok <jaebok.lee@modori.io>
2022-03-02 15:47:21 +0900

    add binance network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -52,6 +52,20 @@ const config: HardhatUserConfig = {
       },
       chainId: 42,
     },
+    binanceTestnet: {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: {
+        mnemonic: process.env.TEST_MNEMONIC,
+      },
+    },
+    bsc: {
+      url: 'https://bsc-dataseed.binance.org/',
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: [process.env.ADMIN || ''],
+    },
     ganache: {
       url: 'http://0.0.0.0:8545',
     },
